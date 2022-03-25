882688d7dcf0caad3a97cdcf08c9237517a7f153
unknown <hung.pnpt93@gmail.com>
2022-03-25 08:14:39 +0700

    th2 thuc hanh static

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Car car1 = new Car("honda","eg3");
+        System.out.println(Car.count);
+        Car car2 = new Car("maybach","eg8");
+        System.out.println(Car.count);
+        Car car3 = new Car("vinFast","eg4");
+        System.out.println(Car.count);
+    }
+}
